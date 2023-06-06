ee90bd22d2f77c39b5cf53a2aac3294f695c74e1
Brandon Icardi <brandonicardi@gmail.com>
2023-06-06 15:58:47 -0300

    first commit

---
+++ src/main/java/com/unla/grupo18/Grupo18Application.java
@@ -0,0 +1,13 @@
+package com.unla.grupo18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo18Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/grupo18/Grupo18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
